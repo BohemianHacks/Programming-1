e6e1817287d3cde96d339acbbf844c5470ef15a0
BohemianHacks <jeff.blecha@gmail.com>
2013-09-09 19:18:29 +0200

    First code for the class :D

---
+++ helloworld.cpp
@@ -0,0 +1,18 @@
+//Hello world program
+//Built to introduce me to C++
+//by Jeff Blecha
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void print(string words){
+	cout << words << endl;
+}
+
+int main(){
+	print("Hello World!");
+	cin.ignore();
+	return 0;
+}
