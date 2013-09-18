95111ba4265c02d30be78cc98189526eb03d4b89
BohemianHacks <jeff.blecha@gmail.com>
2013-09-18 17:52:56 +0200

    circle.cpp

---
+++ circle.cpp
@@ -0,0 +1,28 @@
+//Circle.cpp: program to find an area of a circle
+//By Jeff Blecha 9-18-2013
+
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include "stdafx.h"
+#define _USE_MATH_DEFINES
+
+
+using namespace std;
+
+int main (){
+
+    //declare variables
+    double radius;
+    double area;
+
+    //get input
+    cout << "Radius:";
+    cin >> radius;
+
+    //find area (pi*r^2)
+    area = radius * radius * M_PI;
+
+    //display results
+    cout << "Area:" << area << endl;
+}
